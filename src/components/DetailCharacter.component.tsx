6f6e084d762e72572c34e399028c10b19031bf57
daniel contreras <35605163+daniel2811contreras@users.noreply.github.com>
2023-10-24 00:59:51 -0500

    realy respocive, img detail

--- src/components/DetailCharacter.component.tsx
+++ src/components/DetailCharacter.component.tsx
@@ -31,7 +31,7 @@ const DetailCharacter: React.FC = () => {
         src={CharactersSelected?.image}
         alt={CharactersSelected?.name}
         effect="blur"
-        width="150px"
+        width={window.innerWidth > 480 ? "150px" : "100px"}
         placeholderSrc={CharactersSelected?.image}
       />
       <ContainerColumnDropdownDetailsCharacters>
